f0c0540b0e60b0afc5b8a6a1a8c792a64ef7853d
Dmitrii Kudriavtsev <36885141+armandupe@users.noreply.github.com>
2019-02-24 16:21:32 +0300

    edit

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,11 @@
 // Подключаем Gulp
 var gulp = require("gulp"); 
 
-// Создаем простой таск
-gulp.task('myFirstTask', function() {
- console.log('Привет, я твой первый таск!');
-});
-
-// Запуск тасков по умолчанию
-gulp.task("default", ["myFirstTask"]);
+// Подключаем плагины Gulp
+var sass = require("gulp-sass"), // переводит SASS в CSS
+    cssnano = require("gulp-cssnano"), // Минимизация CSS
+    autoprefixer = require('gulp-autoprefixer'), // Проставлет вендорные префиксы в CSS для поддержки старых браузеров
+    imagemin = require('gulp-imagemin'), // Сжатие изображений
+    concat = require("gulp-concat"), // Объединение файлов - конкатенация
+    uglify = require("gulp-uglify"), // Минимизация javascript
+    rename = require("gulp-rename"); // Переименование файлов
